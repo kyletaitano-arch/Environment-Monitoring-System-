3f1e578c98c54cf9cbeec4671a11d52ab79ecaf9
chaircollector <anthonys1608@gmail.com>
2026-04-07 14:42:44 -0700

    fix branch

--- Sensor/Temperature.py
+++ Sensor/Temperature.py
@@ -1,11 +1,7 @@
 #Kyle was here 
-<<<<<<< HEAD
 # Anthony was here pt.2
-=======
-# Anthony was here
 # hiiiiiiiiiiiiiiiiii Cmp.
 #izzy was here
->>>>>>> fe62ab80a0471fc092695bac5ab235e880972a55
 # import smbus2 as smbus
 import smbus 
 import time
